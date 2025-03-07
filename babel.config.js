63519929e9f5f8a21bc1f77333ac400d394965b8
soham444101 <146668441+soham444101@users.noreply.github.com>
2025-03-07 16:31:31 +0530

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    'babel-preset-expo',
+    // other presets
+  ],
+  plugins: [
+    '@babel/plugin-transform-template-literals',
+    // other plugins
+  ],
+};
